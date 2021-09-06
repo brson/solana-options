f1f530f79279e998b97119d5beb644b38209d548
Brian Anderson <andersrb@gmail.com>
2021-09-06 04:49:00 -0600

    .

--- programs/broker/src/lib.rs
+++ programs/broker/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::clock::Slot;
 
@@ -23,12 +25,36 @@ mod broker {
     /// If the token already exists then this instruction does nothing and
     /// returns the existing token's account.
     pub fn create_contract_token(ctx: Context<CreateContractToken>, desc: ContractTokenDesc) -> Result<Pubkey, ProgramError> {
-        #![allow(unused)]
         panic!()
     }
 
     pub fn get_contract_token(ctx: Context<GetContractToken>, desc: ContractTokenDesc) -> Result<Option<Pubkey>, ProgramError> {
-        #![allow(unused)]
+        panic!()
+    }
+
+
+    /// Mint new options tokens by depositing collateral.
+    ///
+    /// Deposit collateral:
+    ///
+    /// for call contracts:
+    ///
+    /// - `qty` units of the underling (BTC in BTC/USD)
+    ///
+    /// for put contracts:
+    ///
+    /// - `strike_price` units of the base (USD in BTC/USD)
+    ///
+    /// User receives `qty` units of the contract token.
+    pub fn mint_contract_tokens(ctx: Context<MintContractTokens>,
+                                contract_token: Pubkey,
+                                qty: u64) -> ProgramResult {
+        panic!();
+    }
+
+    /// Called by holders of tokens to resolve the outcome of expiration.
+    pub fn redeem_contract_tokens(ctx: Context<RedeemContractTokens>,
+                                  contract_token: Pubkey) -> ProgramResult {
         panic!()
     }
 }
@@ -52,14 +78,31 @@ pub struct GetContractToken<'info> {
 
 #[derive(AnchorSerialize, AnchorDeserialize)]
 pub struct ContractTokenDesc {
-    underlying_serum_market: Pubkey,
-    expiration_slot: Slot,
-    strike_price: u64,
-    kind: ContractKind,
+    pub underlying_serum_market: Pubkey,
+    pub expiration_slot: Slot,
+    pub strike_price: u64,
+    pub kind: ContractKind,
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize)]
 pub enum ContractKind { Call, Put }
+
+#[derive(Accounts)]
+pub struct MintContractTokens<'info> {
+    #[account(mut)]
+    pub broker_account: ProgramAccount<'info, BrokerAccount>,
+    pub user_account: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct RedeemContractTokens<'info> {
+    #[account(mut)]
+    pub broker_account: ProgramAccount<'info, BrokerAccount>,
+    pub user_account: AccountInfo<'info>,
+}
+
+
+
 
 
 
